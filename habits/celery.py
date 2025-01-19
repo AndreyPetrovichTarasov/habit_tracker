2dbeb492790566e41ba9d0a9ee3840296fe3fc2e
lacry <lacryk@gmail.com>
2025-01-19 10:49:23 +0300

    moduls

--- habits/celery.py
+++ habits/celery.py
@@ -1,10 +1,28 @@
+import os
+
 from celery import Celery
+
+# app = Celery("habit_tracker", broker="redis://localhost:6379/0")
+#
+# app.conf.beat_schedule = {
+#     "send_reminders": {
+#         "task": "send_habit_reminder",
+#         "schedule": 60.0,
+#     },
+# }
+
+
+# Укажите путь к настройкам Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery("habit_tracker", broker="redis://localhost:6379/0")
 
-app.conf.beat_schedule = {
-    "send_reminders": {
-        "task": "send_reminders_task",
-        "schedule": 60.0,
-    },
-}
+# Настройки Celery берутся из настроек Django, начиная с префикса CELERY_
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Автоматически обнаруживать задачи в установленных приложениях
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
